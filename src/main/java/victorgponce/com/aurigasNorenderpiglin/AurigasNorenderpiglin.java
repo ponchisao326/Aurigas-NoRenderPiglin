c1566a6dac98f400dbe3f46a7c4fd850e2eefc6e
ponchisao326 <ponchisao326@hotmail.com>
2024-12-12 23:19:41 +0100

    Basicamente todo, es muy básico así que no es mucho

---
+++ src/main/java/victorgponce/com/aurigasNorenderpiglin/AurigasNorenderpiglin.java
@@ -0,0 +1,10 @@
+package victorgponce.com.aurigasNorenderpiglin;
+
+import net.fabricmc.api.ModInitializer;
+
+public class AurigasNorenderpiglin implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/victorgponce/com/aurigasNorenderpiglin/client/AurigasNorenderpiglinClient.java
@@ -0,0 +1,10 @@
+package victorgponce.com.aurigasNorenderpiglin.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AurigasNorenderpiglinClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
